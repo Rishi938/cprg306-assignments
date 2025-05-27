16dd49447763d15779a9973670ba30c6f871503f
Rishi938 <rishichaudhari712@gmail.com>
2025-05-26 18:34:24 -0600

    Week 2 assignment complete

---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Rishi938</h2>
+      <p>
+        GitHub Repo:{" "}
+        <Link href="https://github.com/your-username/cprg306-assignments" target="_blank">
+          View on GitHub
+        </Link>
+      </p>
+    </div>
+  );
+}
